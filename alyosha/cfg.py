a49517ba38bc83cdd23e48e0b19a668e1c736b35
Yuu Okano <okano.yu@po.ntts.co.jp>
2016-06-30 17:07:03 +0900

    simple cfg-parser worked well.

--- alyosha/cfg.py
+++ alyosha/cfg.py
@@ -1,14 +1,22 @@
-from oslo_config import cfg
-from oslo_config import types
+#from oslo_config import cfg
+import oslo_config.cfg
 
-PortType = types.Integer(1, 65535)
+# usage:
+# ./bin/alyosha --list_option 1,2,3 --string_option "test-string" --enable_debugger
 
-common_opts = [
-    cfg.StrOpt('bind_host',
-               default='0.0.0.0',
-               help='IP address to listen on.'),
-    cfg.Opt('bind_port',
-            type=PortType,
-            default=9292,
-            help='Port number to listen on.')
-]
+CONF = oslo_config.cfg.ConfigOpts()
+CONF.register_cli_opts([
+    oslo_config.cfg.ListOpt('list_option', default=[], help=''),
+    oslo_config.cfg.StrOpt('string_option', default=None, help=''),
+    oslo_config.cfg.BoolOpt('enable_debugger', default=False, help=''),
+])
+
+from oslo_config.cfg import ConfigOpts
+from oslo_config.cfg import BoolOpt
+from oslo_config.cfg import IntOpt
+from oslo_config.cfg import ListOpt
+from oslo_config.cfg import MultiStrOpt
+from oslo_config.cfg import StrOpt
+from oslo_config.cfg import FloatOpt
+from oslo_config.cfg import RequiredOptError
+from oslo_config.cfg import ConfigFilesNotFoundError
